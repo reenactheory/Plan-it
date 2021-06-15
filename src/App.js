69fb8f93f26f5d5ad91f354ccb3b393962f4d6be
reenactemotion <jenna.jaeyeon@gmail.com>
2021-06-15 09:26:13 +0900

    A first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import clock from './components/clock.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="App">
+          <div id="header">
+            <div id="texts">
+              <p className="title-text">Plan-it</p>
+              <p className="one-line-text">Always do your best</p>
+            </div>
+            <clock>d</clock>
+        </div>
+      </div>
+    );
+  }
 }
-
+  
 export default App;
